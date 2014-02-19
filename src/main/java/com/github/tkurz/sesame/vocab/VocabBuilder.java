0879dd781c24f02a0024c6b3a222443978999bed
Peter Ansell <p_ansell@yahoo.com>
2014-02-20 09:50:50 +1100

    Use Model API rather than Repository API

--- src/main/java/com/github/tkurz/sesame/vocab/VocabBuilder.java
+++ src/main/java/com/github/tkurz/sesame/vocab/VocabBuilder.java
@@ -1,8 +1,17 @@
 package com.github.tkurz.sesame.vocab;
 
+import org.openrdf.model.Literal;
+import org.openrdf.model.Model;
+import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.util.GraphUtil;
+import org.openrdf.model.util.GraphUtilException;
+import org.openrdf.model.vocabulary.DCTERMS;
 import org.openrdf.model.vocabulary.OWL;
 import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.model.vocabulary.SKOS;
 import org.openrdf.query.*;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
@@ -11,10 +20,13 @@ import org.openrdf.repository.RepositoryResult;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.Rio;
 import org.openrdf.sail.memory.MemoryStore;
 
 import java.io.*;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,96 +38,90 @@ import java.util.regex.Pattern;
  */
 public class VocabBuilder {
 
-    private Repository repository;
-
     File file;
     String name;
     RDFFormat rdfFormat;
     String prefix = "http://example.org/ontology#";
     String outputFolder = "/tmp";
     String packageName = "org.apache.marmotta.commons.vocabulary";
+	private Model model;
 
     public VocabBuilder(String filename, RDFFormat format) throws IOException, RepositoryException, RDFParseException, MalformedQueryException, QueryEvaluationException {
         this.file = new File(filename);
         if(!this.file.exists()) throw new FileNotFoundException();
         this.rdfFormat = format;
 
-        //parse
-        repository = new SailRepository(new MemoryStore());
-        repository.initialize();
-        RepositoryConnection connection = repository.getConnection();
+        model = Rio.parse(new FileInputStream(file), "", rdfFormat);
 
         //import
-        connection.add(file,null,rdfFormat);
-        RepositoryResult<Statement> results = connection.getStatements(null, RDF.TYPE, OWL.ONTOLOGY, true);
-        if(results.hasNext()) {
-            prefix = results.next().getSubject().stringValue();
+        Set<Resource> owlOntologies = model.filter(null, RDF.TYPE, OWL.ONTOLOGY).subjects();
+        if(!owlOntologies.isEmpty()) {
+        	prefix = owlOntologies.iterator().next().stringValue();
         }
-
+        
         name = this.file.getName();
 
         if(name.contains(".")) name = name.substring(0,name.lastIndexOf("."));
         name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
 
-        connection.close();
     }
 
-    public void run() throws RepositoryException, IOException, RDFParseException, MalformedQueryException, QueryEvaluationException {
-
-        RepositoryConnection connection = repository.getConnection();
-
-        TupleQuery tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, "SELECT ?a ?b ?c {?a ?b ?c.}");
-
-        TupleQueryResult results = tupleQuery.evaluate();
+    public void run() throws IOException, GraphUtilException {
 
         Pattern pattern = Pattern.compile(Pattern.quote(prefix)+"(.+)");
-
-        HashMap<String,String> vals = new HashMap<String,String>();
-        while(results.hasNext()) {
-            BindingSet bs = results.next();
-            Matcher matcher = pattern.matcher(bs.getValue("a").stringValue());
-            if(matcher.find()) {
-                String k = cleanKey(matcher.group(1));
-                vals.put(k, matcher.group(0));
-            }
+        HashMap<String,URI> splitUris = new HashMap<String, URI>();
+        for(Resource nextSubject : model.subjects()) {
+        	if(nextSubject instanceof URI) {
+	            Matcher matcher = pattern.matcher(nextSubject.stringValue());
+	            if(matcher.find()) {
+	                String k = cleanKey(matcher.group(1));
+	                splitUris.put(k, (URI)nextSubject);
+	            }
+        	}
         }
 
         //print
-        FileWriter w = new FileWriter(outputFolder+"/"+name+".java");
-        PrintWriter out = new PrintWriter(w);
-        out.printf("package %s;",packageName);
-        out.printf("\n\nimport org.openrdf.model.URI;\n" +
-                "import org.openrdf.model.ValueFactory;\n" +
-                "import org.openrdf.model.impl.ValueFactoryImpl;\n\n");
-        out.printf("/** \n * Namespace %s\n */\npublic class %s {\n\n",name,name);
-        out.printf("\tpublic static final String NAMESPACE = \"%s\";\n\n",prefix);
-        out.printf("\tpublic static final String PREFIX = \"%s\";\n\n",name.toLowerCase());
-
-        TreeSet<String> keys = new TreeSet(vals.keySet());
-
-        for(String key : keys) {
-            tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, "SELECT ?a WHERE {{<"+vals.get(key)+"> <http://www.w3.org/2000/01/rdf-schema#comment> ?a} UNION {<"+vals.get(key)+"> <http://purl.org/dc/terms/description> ?a} UNION {<"+vals.get(key)+"> <http://www.w3.org/2004/02/skos/core#definition> ?a}}");
-            results = tupleQuery.evaluate();
-            if(results.hasNext()) {
-                out.printf("\t/**\n\t * %s \n\t */\n", results.next().getValue("a").stringValue());
-            }
-            out.printf("\tpublic static final URI %s;\n\n",key);
+        try(final FileWriter w = new FileWriter(outputFolder+"/"+name+".java");
+        		final PrintWriter out = new PrintWriter(w);)
+        {
+	        out.printf("package %s;",packageName);
+	        out.printf("\n\nimport org.openrdf.model.URI;\n" +
+	                "import org.openrdf.model.ValueFactory;\n" +
+	                "import org.openrdf.model.impl.ValueFactoryImpl;\n\n");
+	        out.printf("/** \n * Namespace %s\n */\npublic class %s {\n\n",name,name);
+	        out.printf("\tpublic static final String NAMESPACE = \"%s\";\n\n",prefix);
+	        out.printf("\tpublic static final String PREFIX = \"%s\";\n\n",name.toLowerCase());
+	
+	        TreeSet<String> keys = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+	        keys.addAll(splitUris.keySet());
+	        
+	        for(String key : keys) {
+	        	Literal comment = GraphUtil.getOptionalObjectLiteral(model, splitUris.get(key), RDFS.COMMENT);
+	        	if(comment == null) {
+	        		comment = GraphUtil.getOptionalObjectLiteral(model, splitUris.get(key), DCTERMS.DESCRIPTION);
+	        	}
+	        	if(comment == null) {
+	        		comment = GraphUtil.getOptionalObjectLiteral(model, splitUris.get(key), SKOS.DEFINITION);
+	        	}
+	            if(comment != null) {
+	                out.printf("\t/**\n\t * %s \n\t */\n", comment.getLabel());
+	            } else {
+	                out.printf("\t/**\n\t * %s \n\t */\n", key);
+	            }
+	            out.printf("\tpublic static final URI %s;\n\n",key);
+	        }
+	
+	        out.printf("\n\tstatic{\n\t\tValueFactory factory = ValueFactoryImpl.getInstance();");
+	        for(String key : keys) {
+	
+	            out.printf("\n\t\t%s = factory.createURI(%s, \"%s\");",key,name+".NAMESPACE",key);
+	        }
+	
+	        out.println("\n\t}\n}");
+	
+	        //end
+	        out.flush();
         }
-
-        out.printf("\n\tstatic{\n\t\tValueFactory factory = ValueFactoryImpl.getInstance();");
-        for(String key : keys) {
-
-            out.printf("\n\t\t%s = factory.createURI(%s, \"%s\");",key,name+".NAMESPACE",key);
-        }
-
-        out.println("\n\t}\n}");
-
-        //end
-        out.flush();
-        out.close();
-        w.close();
-        connection.close();
-        repository.shutDown();
     }
 
     private String cleanKey(String s) {

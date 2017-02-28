f49c861d2bf1cbe155f05fda15872162b86d5d47
Peter Ansell <p_ansell@yahoo.com>
2017-02-28 16:45:31 +1100

    issue #8 : Add Schema and SchemaRegistry as reference points
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ core/src/main/java/com/github/ansell/rdf4j/schemagenerator/Schema.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.github.ansell.rdf4j.schemagenerator;
+
+/**
+ * A high level interface used to denote the vital details of a schema and
+ * enable a reference for registry based auto-discovery.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public interface Schema {
+
+}
